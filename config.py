3321b4a6e662773c3f5e42373251a54e3c5c0c37
Arsene <amoyaarsene@gmail.com>
2026-07-14 02:01:05 +0100

    first commit

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ def get_config():
 
     # common settings
     parser.add_argument("--backbone", type=str, default="resnet50_tanh", help="see network.py")
-    parser.add_argument("--data-dir", type=str, default="../_datasets", help="directory to dataset")
+    parser.add_argument("--data-dir", type=str, default="../data", help="directory to dataset")
     parser.add_argument("--n-workers", type=int, default=4, help="number of dataloader workers")
     parser.add_argument("--n-epochs", type=int, default=120, help="number of epochs to train for")
     parser.add_argument("--batch-size", type=int, default=32, help="input batch size")
@@ -19,8 +19,8 @@ def get_config():
     parser.add_argument("--parallel-val", type=bool, default=True, help="use a separate thread for validation")
 
     # changed at runtime
-    parser.add_argument("--dataset", type=str, default="cifar", help="cifar/nuswide/flickr/coco")
-    parser.add_argument("--n-classes", type=int, default=10, help="number of dataset classes")
+    parser.add_argument("--dataset", type=str, default="flickr", help="cifar/nuswide/flickr/coco")
+    parser.add_argument("--n-classes", type=int, default=38, help="number of dataset classes")
     parser.add_argument("--topk", type=int, default=None, help="mAP@topk")
     parser.add_argument("--save-dir", type=str, default="./output", help="directory to output results")
     parser.add_argument("--n-bits", type=int, default=128, help="length of hashing binary")
